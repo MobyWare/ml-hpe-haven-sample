6c14a6ae9afb10c84a1e84db74f14295866c6d1b
dickm <dickm@upmc.edu>
2016-07-11 08:59:53 -0400

    Working version with env for key.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').load() //seems this is needed to load environment variables
+
 var path = require('path')
 var express = require('express')
 var app = express()
@@ -7,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 //Reference haven api
 var havenondemand = require('havenondemand')
-var client = new havenondemand.HODClient('<your API>')
+var client = new havenondemand.HODClient(process.env.HOD_APP_KEY_DEV) //store API key in environment variable
 
 var port = process.env. PORT || 5000
 
@@ -19,49 +21,29 @@ app.get(
   }
 )
 
-app.get(
-  '/process',
-  function(req, res){
-    var url = req.query.url
-    var data = {'url':url}
-
-    //Make the call to HOD.
-    client.call(
-      'analyzesentiment',
-      data,
-      function(err, resp, body){
-        if(err){
-          console.log(err)
+app.get('/process', function(req, res) {
+  var url = req.query.url
+  var data = {url: url}
+  client.call('extractconcepts', data, function(err1, resp1, body1) {
+    if (err1) {
+      console.log(err1)
+      res.sendStatus(500)
+    } else {
+      console.log('Extracted concepts')
+      var conceptsPayload = resp1.body
+      client.call('analyzesentiment', data, function(err2, resp2, body2) {
+        if (err2) {
+          console.log(err2)
           res.sendStatus(500)
+        } else {
+          console.log('Extracted sentiments')
+          var sentimentsPayload = resp2.body
+          res.status(200).json({concepts: conceptsPayload, sentiments: sentimentsPayload})
         }
-        else{
-          console.log('Analyzed sentiment!')
-          var sentiments = resp.body
-        }
-
-        //Now we call the concept extraction api
-        client.call(
-          'extractconcepts',
-          data,
-          function(err, resp, body){
-            if(err){
-              console.log(err)
-              res.sendStatus(500)
-            }
-            else{
-              console.log('Extracted concepts!')
-              var concepts = resp.body
-              res.sendStatus(200).json({'sentiments': sentiments, 'concepts':concepts})
-            }
-          }
-        )
-
-      }
-    )
-
-
-  }
-)
+      })
+    }
+  })
+})
 
 //Start app on port given
 app.listen(
